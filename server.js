6f6a902b33b732ca7ed24e2c28b7d78022f5709f
Sabah <sabahalmel7@gmail.com>
2023-12-05 10:57:22 +0200

    put the login as get

--- server.js
+++ server.js
@@ -37,7 +37,7 @@ app.use("/campaigns", campaignRouter);
 app.use("/categories", categoryRouter);
 app.use("/donors", donorRouter);
 app.use("/donations", donationRouter);
-app.post("/login", signIn);
+app.get("/login", signIn);
 app.get("/protected", authorize, (req, res) => {
   return res.json({ user: { id: req.userId, role: req.userRole } });
 });
